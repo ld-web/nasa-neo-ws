2d65c3360945bc048c4a4c13bcd87b2768efdc78
Lucas Delobelle <lucas@ld-web.net>
2019-11-26 16:15:45 +0100

    react-datepicker, NeoWsContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './Nav';
+import NeoWsContainer from './NeoWs/NeoWsContainer';
 
 function App() {
   return (
     <>
       <Nav />
+      <NeoWsContainer />
     </> 
   );
 }
