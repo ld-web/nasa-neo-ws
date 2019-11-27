c6491f8b97afc432de9a20996e8da0848e842b69
Lucas Delobelle <lucas@ld-web.net>
2019-11-27 10:48:11 +0100

    documentation in components

--- src/components/NeoWs/NeoWsContainer.js
+++ src/components/NeoWs/NeoWsContainer.js
@@ -6,6 +6,14 @@ import moment from "moment";
 import NeoWsError from "./NeoWsError";
 
 const NeoWsContainer = () => {
+  // On pourrait déclarer une variable d'état structurée
+  // useState prend en paramètre la valeur d'initialisation de la variable d'état
+  // Cela signifie que dateInterval, à la ligne suivante, vaudra l'objet passé en paramètre
+  // const [dateInterval, setDateInterval] = useState({
+  //   startDate: new Date(),
+  //   endDate: new Date()
+  // });
+
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [error, setError] = useState(null);
@@ -23,14 +31,28 @@ const NeoWsContainer = () => {
     }
   };
 
+  // useEffect permet de déclencher ce qu'on appelle des "effets de bord"
+  // Lors du changement d'une variable d'état (par exemple), on pourra lancer une fonction
+  // Cette fonction à lancer est le premier paramètre fourni.
+  // Le second paramètre, quant à lui, est un tableau contenant les dépendances, c'est-à-dire les variables qui, quand elles changeront, déclencheront l'appel de la fonction.
   useEffect(() => {
     try {
+      console.log("useEffect des dates");
       checkInterval(startDate, endDate);
       setError(null);
     } catch (e) {
       setError({message: e.message});
     }
   }, [startDate, endDate]);
+
+  // On peut déclarer un effet de bord qui ne sera exécuté qu'au montage du composant.
+  // Pour faire ça, on déclare notre fonction avec un tableau de dépendances vide.
+  // Ainsi, la fonction sera exécutée au montage du composant, mais n'aura aucune dépendance en cas de modification de variable d'état.
+  // Elle n'aura donc aucune raison de s'exécuter de nouveau ensuite.
+  // C'est l'équivalent de la méthode componentDidMount dans un composant classe
+  useEffect(() => {
+    console.log("Je suis monté !")
+  }, []);
 
   return (
     <div className="uk-container">
@@ -43,6 +65,10 @@ const NeoWsContainer = () => {
           selected={startDate}
           className="uk-input"
           onChange={date => setStartDate(date)}
+          // ici onChange est équivalent à :
+          // onChange={function(date) {
+          //    setStartDate(date);
+          // }}
         />
         Date de fin :
         <DatePicker
--- src/components/NeoWs/NeoWsError.js
+++ src/components/NeoWs/NeoWsError.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Alert from '../Utils/Alert';
 
 const NeoWsError = ({ error }) => (
-  error && <Alert type="danger" message={error.message} />
+    error && <Alert type="danger" message={error.message} />
 );
 
 export default NeoWsError;
