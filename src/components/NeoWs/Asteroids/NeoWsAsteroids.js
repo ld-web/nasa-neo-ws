3e4a6e0f7a286d14a13580720f9344e18cd85e06
Lucas Delobelle <lucas@ld-web.net>
2019-11-29 09:37:09 +0100

    Asteroid dates sorted

--- src/components/NeoWs/Asteroids/NeoWsAsteroids.js
+++ src/components/NeoWs/Asteroids/NeoWsAsteroids.js
@@ -5,14 +5,25 @@ const NeoWsAsteroids = ({ asteroidsCollection }) => {
   const asteroidsDates = Object.keys(asteroidsCollection);
   console.log("Dates des astéroïdes : ", asteroidsDates);
 
+  // sort va modifier la position des éléments dans notre tableau
+  // slice permet donc de faire une copie du tableau puis un tri
+  const sortedAsteroidDates = asteroidsDates.slice().sort((a, b) => {
+    let dateA = new Date(a);
+    let dateB = new Date(b);
+
+    return dateA - dateB;
+  });
+
   return (
     <div>
-      {asteroidsDates.map(asteroidsDate => (
-        <div>
+      {sortedAsteroidDates.map(asteroidsDate => (
+        <div key={asteroidsDate}>
           <h2>{asteroidsDate}</h2>
           <div>
             {asteroidsCollection[asteroidsDate].map(asteroid => (
-              <div key={asteroid.id}>{asteroid.name}</div>
+              <div key={asteroid.id}>
+                {asteroid.name}
+              </div>
             ))}
           </div>
         </div>
